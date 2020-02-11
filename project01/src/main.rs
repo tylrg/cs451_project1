547f31d07002597dc066974d566b8066d505f214
Tyler Gabriel <tygabriel7@gmail.com>
2020-02-11 10:22:51 -0500

    Added bit twiddling functions

--- project01/src/main.rs
+++ project01/src/main.rs
@@ -39,6 +39,46 @@ fn write_message(filename: &str,message: &str){
     println!("Embedding the message {} in the file {}",message,filename)
 }
 
-fn read_as_byte(){
+fn read_byte_by_byte(path: &str)->Result<Vec<u8>, io:Error>{
+    println!("Fuck")
+}
 
+fn set_bit(byte: u8, position: u8)->u8{
+    match position{
+        0 => byte | 0b1000_0000,
+        1 => byte | 0b0100_0000,
+        2 => byte | 0b0010_0000,
+        3 => byte | 0b0001_0000,
+        4 => byte | 0b0000_1000,
+        5 => byte | 0b0000_0100,
+        6 => byte | 0b0000_0010,
+        7 => byte | 0b0000_0001,
+        _ => panic!("Uh, dude, what are you doing?")
+    }
+}
+fn unset_bit(byte: u8,position: u8)-> u8{
+    match position{
+        0 => byte & 0b0111_1111,
+        1 => byte & 0b1011_1111,
+        2 => byte & 0b1101_1111,
+        3 => byte & 0b1110_1111,
+        4 => byte & 0b1111_0111,
+        5 => byte & 0b1111_1011,
+        6 => byte & 0b1111_1101,
+        7 => byte & 0b1111_1110,
+        _ => panic!("Uh, dude, what are you doing?")
+    }
+}
+fn toggle_bit(byte: u8, position: u8)->u8{
+    match position{
+        0 => byte ^ 0b1000_0000,
+        1 => byte ^ 0b0100_0000,
+        2 => byte ^ 0b0010_0000,
+        3 => byte ^ 0b0001_0000,
+        4 => byte ^ 0b0000_1000,
+        5 => byte ^ 0b0000_0100,
+        6 => byte ^ 0b0000_0010,
+        7 => byte ^ 0b0000_0001,
+        _ => panic!("Uh, dude, what are you doing?")
+    }
 }
