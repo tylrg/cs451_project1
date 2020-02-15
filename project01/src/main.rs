60ec6fd2356b0226f58165b1674a7209a0b1253f
Tyler Gabriel <tygabriel7@gmail.com>
2020-02-14 23:37:14 -0500

    Completed reading, working on writing!

--- project01/src/main.rs
+++ project01/src/main.rs
@@ -16,23 +16,28 @@ fn main() -> std::io::Result<()> {
             eprintln!("Need to have at least one argument")//reject
         },
         2=>{
-            //find_message(&args[1]);//find the message in the specified file
+            read_byte_by_byte(&args[1])?;
+            return Ok(());
         },
-        3=>write_message(&args[1],&args[2]),//hide the messages in the specified file
+        3=>{
+            write_message(&args[1],&args[2])?;
+            return Ok(());
+        },//hide the messages in the specified file
         _=>eprintln!("Need to have at most two arguments"),//default case
     }
 
-    let _ret_val = read_byte_by_byte(&args[1])?;
+    //let _ret_val = read_byte_by_byte(&args[1])?;
     Ok(())//got to the end of the file and yeah it might work
 }
 
 fn read_byte_by_byte(path: &str)-> Result<Vec<u8>,io::Error>{
+    //establishing variables necessary for method
     println!("File path: {}",path);
     let mut f = fs::File::open(path)?;
-    let mut bytes = vec![0u8,0];
-    let mut byte_buffer = [0u8,8];
+    let mut bytes = vec![0u8,0];//vector of all of the bytes
+    let mut byte_buffer = [0u8,8];//bytes being read in
 
-    let mut i = 0;
+    //looping through input file and reading bytes into buffer and then adding to bytes
     loop{
         match f.read(&mut byte_buffer)?{
             num_bytes_read if num_bytes_read == 0 =>{
@@ -40,70 +45,87 @@ fn read_byte_by_byte(path: &str)-> Result<Vec<u8>,io::Error>{
             },
             num_bytes_read if num_bytes_read > 0 =>{
                 bytes.extend(&byte_buffer[..num_bytes_read]);
-                //println!("{:x?} {:x?}",&byte_buffer[..1],&byte_buffer[1..num_bytes_read]);
             },
             _ =>{
-                panic!("What the fuck is happening"); 
+                panic!("Failed to read in bytes!"); 
             }
         }
-        i+=1;
     }
 
-
-    i = 0;
-    let mut binary_values = vec![0u8,0];
-    let mut message = vec![0 as char,'0'];
+    let mut binary_values = vec![0u8,0];//binary values to print, can remove later?
+    let mut message = vec![0 as char,'0'];//character form of the binary values
+    binary_values.pop();//idk why but these arrays have two elements
+    binary_values.pop();
+    message.pop();
+    message.pop();
     let mut index = 0;
-    let mut binary:u8 = 0000_0000;
-    let mut nl_count = 0;
+    let mut binary:u8 = 0000_0000;//default binary value
+    let mut nl_count = 0;//number of newlines found in header
+
+    
     for &byte in &bytes{
         if nl_count >=3{
-            
-            if byte%2 != 0 {
-                binary = set_bit(binary,index);
-            }
-            //print!("{:x?} ",byte);
-            index+=1;
+            if byte%2 != 0 {binary = set_bit(binary,index);}//if the byte is odd, set the corresponding bit to 1
+            index+=1;//increment the index of which bit we are looking at
+
             if index%8 == 0{
                 binary_values.push(binary);
-                
-                let s = format!("{:b}",binary);
-                println!("VALUE ADDED TO LIBRARY: {}" , s);
                 let ch = binary as char;
-                let hex = format!("{:x}",binary);
                 message.push(ch);
                 if binary == 0{
-                    //message.push('0');
                     println!("Message :{:?}",message);
                     println!("Binary Values: {:?}",binary_values);
                     break;
                 }
-                println!("{} {} {}",s,hex,ch);
                 index=0;
                 binary=0;
             }
         }   
 
-        if byte == 10 {
-            nl_count+=1;
-            println!("FOUND A NEWLINE");
-        }
+        //checking if the byte is equal to newline
+        if byte == 10 {nl_count+=1;}
     }
-
-
-    
 
     Ok(bytes)
 }
 
-//takes in a string literal name of the file
-// fn find_message(filename: &str){
-//     println!("Finding the message in the file {}",filename);
-// }
-
 //takes in a string literal name of the file and a string literal message
-fn write_message(filename: &str,message: &str){
-    println!("Embedding the message {} in the file {}",message,filename)
+fn write_message(path: &str,message: &str)-> Result<Vec<u8>,io::Error>{
+    println!("File path: {}",path);
+    let mut f = fs::File::open(path)?;
+    let mut bytes = vec![0u8,0];//vector of all of the bytes
+    let mut byte_buffer = [0u8,8];//bytes being read in
+
+
+    let mut message_bytes = vec![0u8,0];
+    let mut binary_string = vec![0u8,0];
+    for x in message.chars(){
+        print!("{} ",x);
+        let message_binary = x as u8;
+        message_bytes.push(message_binary);
+        println!("{}",message_binary);
+    }
+    //looping through input file and reading bytes into buffer and then adding to bytes
+    loop{
+        match f.read(&mut byte_buffer)?{
+            num_bytes_read if num_bytes_read == 0 =>{
+                break;
+            },
+            num_bytes_read if num_bytes_read > 0 =>{
+                bytes.extend(&byte_buffer[..num_bytes_read]);
+            },
+            _ =>{
+                panic!("Failed to read in bytes!"); 
+            }
+        }
+    }
+
+    for  &mut byte in &bytes{
+        println!("{}",byte as char);
+    }
+
+
+    Ok(bytes)
 }
 
 
