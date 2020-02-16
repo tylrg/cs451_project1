bdfaeae0b0be5f34e57ef12b4c2a5bf992b86384
Tyler Gabriel <tygabriel7@gmail.com>
2020-02-16 11:24:21 -0500

    Functioning program
    works with string literal as second argument, need to check file path methods

--- project01/src/main.rs
+++ project01/src/main.rs
@@ -49,8 +49,6 @@ fn read_byte_by_byte(path: &str)-> Result<Vec<u8>,io::Error>{
         }
     }
 
-
-
     let mut binary_values = vec![0u8,0];//binary values to print, can remove later?
     let mut message = vec![0 as char,'0'];//character form of the binary values
     binary_values.pop();//idk why but these arrays have two elements
@@ -72,8 +70,6 @@ fn read_byte_by_byte(path: &str)-> Result<Vec<u8>,io::Error>{
                 let ch = binary as char;
                 message.push(ch);
                 if binary == 0{
-                    println!("Message :{:?}",message);
-                    println!("Binary Values: {:?}",binary_values);
                     break;
                 }
                 index=0;
@@ -85,6 +81,10 @@ fn read_byte_by_byte(path: &str)-> Result<Vec<u8>,io::Error>{
         if byte == 10 {nl_count+=1;}
     }
 
+    //let mut output:
+    binary_values.push(10);
+    io::stdout().write(&binary_values)?;
+
     Ok(bytes)
 }
 
@@ -93,20 +93,24 @@ fn write_message(path: &str,message: &str)-> Result<Vec<u8>,io::Error>{
     
     let mut f = fs::File::open(path)?;
     let mut bytes = vec![0u8,0];//vector of all of the bytes
+    bytes.pop();
+    bytes.pop();
+    //println!("{}",bytes.len());
     let mut byte_buffer = [0u8,8];//bytes being read in
 
-    let mut message_bytes = vec![0u8,0];
+    let mut message_bytes = vec![0u8,0];//values of chars as number
     let mut binary_string = vec![0u8,0];
     message_bytes.pop();
     message_bytes.pop();
     binary_string.pop();
     binary_string.pop();
     for x in message.chars(){
-        print!("{} ",x);
+        //print!("{} ",x);
         let message_binary = x as u8;
         message_bytes.push(message_binary);
-        println!("{}",message_binary);
+        //println!("{}",message_binary);
     }
+    message_bytes.push(0);
     //looping through input file and reading bytes into buffer and then adding to bytes
     loop{
         match f.read(&mut byte_buffer)?{
@@ -122,69 +126,65 @@ fn write_message(path: &str,message: &str)-> Result<Vec<u8>,io::Error>{
         }
     }
 
+    //println!("Value of Message Bytes: {:?}",message_bytes);
     // for byte in &mut bytes{
     //     *byte = 0u8;
     // }
 
     let mut nl_count:u8 = 0;
-    let mut input_index:u8 = 0;
+    let mut create_letter_index:u8 = 0;
     let mut message_index:u8 = 0;
-
-    println!("Length of Input String: {}",message_bytes.len());
-    println!("Message bytes: {:?}",message_bytes);
-    
+    let final_index:usize = message.len()+1;
     for byte in &mut bytes{
+        
         //bytes is the array
         //byte is the current byte we are reading in from the array
-        //message index is the current character that is being indexed
+        //message index is the current character that is being encoded
+        //create_letter_index is the bit in the character that we are manipulating
+        //encode_char is the character we are encoding
 
         if nl_count>=3 {
-            if message_index == message_bytes.len() as u8 {break;}
-            let overwrite = message_bytes[message_index as usize];
-            println!("{:?} to {:?}",byte,overwrite);
+            if message_index == final_index as u8 {break;}
+            let encode_char = message_bytes[message_index as usize];
             
             //input char is even
-            let overwrite_val = check_bit(overwrite,message_index);
-            let o_string = format!("{:b}",overwrite_val);
-            println!("{}",o_string);
+            let encode_char_val = check_bit(encode_char,create_letter_index);
 
-            if overwrite_val==0{
+            if encode_char_val==0{
                 //current byte is odd
                 if *byte %2 != 0{
-                    println!("Doing something");
+                    //println!("Doing something");
+                    *byte = unset_bit(*byte,7);
                 }
-                else{println!("Doing nothing");}
+                else{
+                    //println!("Doing nothing");
+                }
             }
-            else if overwrite > 0{
+            else if encode_char > 0{
                 //current byte is even
                 if *byte%2==0{
-                    println!("Doing something");
+                    //println!("Doing something");
+                    *byte = set_bit(*byte,7);
                 }
-                else{println!("Doing nothing");}
+                else{
+                    //println!("Doing nothing");
+                }
             }
-
-
-
-
-
-            message_index+=1;
-
+            create_letter_index+=1;
             //done with a character
-            if message_index == 8{
-                message_index=0;
-                input_index+=1;
+            if create_letter_index == 8{
+                create_letter_index=0;
+                message_index+=1;
 
             }
-            println!("\n");
         }
         if *byte == 10 {nl_count+=1;}
+
     }
 
-
+    io::stdout().write(&bytes)?;
     Ok(bytes)
 }
-
-
 
 fn set_bit(byte: u8, position: u8)->u8{
     match position{
@@ -212,21 +212,8 @@ fn unset_bit(byte: u8,position: u8)-> u8{
         _ => panic!("Uh, dude, what are you doing?")
     }
 }
-// fn toggle_bit(byte: u8, position: u8)->u8{
-//     match position{
-//         0 => byte ^ 0b1000_0000,
-//         1 => byte ^ 0b0100_0000,
-//         2 => byte ^ 0b0010_0000,
-//         3 => byte ^ 0b0001_0000,
-//         4 => byte ^ 0b0000_1000,
-//         5 => byte ^ 0b0000_0100,
-//         6 => byte ^ 0b0000_0010,
-//         7 => byte ^ 0b0000_0001,
-//         _ => panic!("Uh, dude, what are you doing?")
-//     }
-// }
 fn check_bit(byte: u8,position: u8)-> u8{
-    println!("Checking: {}",byte);
+    //println!("Checking: {}",byte);
     match position{
         0 => byte & 0b1000_0000,
         1 => byte & 0b0100_0000,
